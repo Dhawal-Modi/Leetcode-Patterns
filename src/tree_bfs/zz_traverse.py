b3489681ad40497212785c4dc1a9eeff15155c8f
Dhawal Modi <dmodi2@ucmerced.edu>
2025-01-11 12:56:49 -0800

    Update ZigZag traversal code

--- src/tree_bfs/zz_traverse.py
+++ src/tree_bfs/zz_traverse.py
@@ -9,5 +9,40 @@ class Solution:
     def traverse(self, root):
         result = []
 
-    return result
+        q = deque()
+        q.append(root)
+        lr = True
+        while q:
+            levelsize = len(q)
+            currentlevel = deque()
+            for i in range(levelsize):
+                current_node = q.popleft()
+                current_val = current_node.val
 
+                if lr:
+                    currentlevel.append(current_val)
+                else:
+                    currentlevel.appendleft(current_val)
+
+                if current_node.left:
+                    q.append(current_node.left)
+
+                if current_node.right:
+                    q.append(current_node.right)
+
+
+            result.append(list(currentlevel))
+            lr = not lr
+
+        return result
+
+if __name__ == "__main__":
+    sol = Solution()
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
+    root.right.left = TreeNode(6)
+    root.right.right = TreeNode(7)
+    print("Level order traversal: " + str(sol.traverse(root)))
